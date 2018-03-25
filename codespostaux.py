a42a2cd7755fe5e0666c066eea13bb934507906a
Jenny <jenny-lyon@outlook.com>
2018-03-25 16:52:59 +0200

    Initial commit

---
+++ codespostaux.py
@@ -0,0 +1,19 @@
+import urllib.request
+import regex
+
+
+def get_webpage(n):
+    page = urllib.request.urlopen("https://www.codespostaux.com/home/r.php?q="+str(n)+"&Pays=FR").read(10000)
+    return str(page)
+
+def get_tags(n):
+    titleList = regex.findall("<b>"+str(n)+" </b></td><td valign=\"top\"><b>(.*)</b>", get_webpage(n), overlapped=True)
+    return titleList
+
+def get_villes(n):
+    for i in get_tags(n):
+        nom_brut = i.split('<')[0]
+        newstr = nom_brut.replace("\\'", "'")
+        print(newstr)
+
+get_villes(74300)
